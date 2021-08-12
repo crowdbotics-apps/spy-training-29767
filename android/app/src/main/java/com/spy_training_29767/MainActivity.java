421ad7dee55b2d8ad4632edd38869ec7df031ce8
Crowdbotics <team@crowdbotics.com>
2021-08-12 20:07:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/spy_training_29767/MainActivity.java
@@ -0,0 +1,15 @@
+package com.spy_training_29767;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "spy_training_29767";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "spy_training_29767Identifier",
+  url: "https://spy_training_29767Identifier.botics.co",
+  api: "https://spy_training_29767Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
